2f0981be7b7434807d66047774cda4752f066ce6
AlonsoCaroline <ca.caroline.alonso.pro@gmail.com>
2023-10-11 06:46:19 +0200

    locations_items_updated_with_some_creative_ideas

--- server/data_init/items.js
+++ server/data_init/items.js
@@ -2,9 +2,101 @@ export function itemsInit() {
     return [
         {
             "id" :"ITEMID_SAV0_item:0",
-            "name" : "Water flask",
-            "description" : "",
-            "effectCode" : "i-w-20-1",
+            "name" : "Water",
+            "description" : "The most essential resource for survival, because without it you die !",
+            "effectCode" : "",
+            "spriteCode" : "water-image"
+        },
+        {
+            "id" :"ITEMID_SAV0_item:1",
+            "name" : "Food",
+            "description" : "Supplies, including non-perishable goods, necessary for sustenance during the journey, like water, without it you die!",
+            "effectCode" : "",
+            "spriteCode" : "food-image"
+        },
+        {
+            "id" :"ITEMID_SAV0_item:2",
+            "name" : "Clothing",
+            "description" : "Robust garments suitable for different weather conditions and protection, it\'s not just for style !",
+            "effectCode" : "",
+            "spriteCode" : "clothing-image"
+        },
+        {
+            "id" :"ITEMID_SAV0_item:3",
+            "name" : "Weapons",
+            "description" : "Firearms for hunting and protection against wildlife and potential threats. Kill or be killed, that\'s the question. Pow.",
+            "effectCode" : "",
+            "spriteCode" : "weapons-image"
+        },
+        {
+            "id" :"ITEMID_SAV0_item:4",
+            "name" : "Ammunition",
+            "description" : "Ammunition required for firearms. It\'s better if the weapons are loaded, now you know.",
+            "effectCode" : "",
+            "spriteCode" : "ammunition-image"
+        },
+        {
+            "id" :"ITEMID_SAV0_item:5",
+            "name" : "Lighting",
+            "description" : "Sources of light, such as oil lamps, candles, or lanterns, for nighttime visibility. May you see the right path.",
+            "effectCode" : "",
+            "spriteCode" : "lighting-image"
+        },
+        {
+            "id" :"ITEMID_SAV0_item:6",
+            "name" : "Wagon",
+            "description" : "A means of transport for carrying goods and supplies. You will travel in it on the old town road.",
+            "effectCode" : "",
+            "spriteCode" : "wagon-image"
+        },
+        {
+            "id" :"ITEMID_SAV0_item:7",
+            "name" : "Draft Animals",
+            "description" : "Oxen, mules, or horses used to pull wagons or plow fields. Liked that you will be less tired.",
+            "effectCode" : "",
+            "spriteCode" : "draft-animals-image"
+        },
+        {
+            "id" :"ITEMID_SAV0_item:8",
+            "name" : "Repair equipment",
+            "description" : "Tools and materials for making repairs to equipment and wagons. Useful if something break.",
+            "effectCode" : "",
+            "spriteCode" : "repair-equipment-image"
+        },
+        {
+            "id" :"ITEMID_SAV0_item:9",
+            "name" : "Tents",
+            "description" : "Shelter from the elements and for rest during the journey. Perfect for sleeping under the stars.",
+            "effectCode" : "",
+            "spriteCode" : "tents-image"
+        },
+        {
+            "id" :"ITEMID_SAV0_item:10",
+            "name" : "First aid kit",
+            "description" : "Medical supplies for basic healthcare needs and first aid. Vous will realize how vital it is when you need it.",
+            "effectCode" : "",
+            "spriteCode" : "first-aid-kit-image"
+        },
+        {
+            "id" :"ITEMID_SAV0_item:11",
+            "name" : "Hygiene items",
+            "description" : "Personal hygiene items such as soap, toothbrush, and toilet paper. Bye bye bad odors.",
+            "effectCode" : "",
+            "spriteCode" : "hygiene-items-image"
+        },
+        {
+            "id" :"ITEMID_SAV0_item:12",
+            "name" : "Cooking equipment",
+            "description" : "Utensils, pots, and pans for cooking and meal preparation. Ideal to make delicious meals, don't forget to invite me for dinner when you've arrived",
+            "effectCode" : "",
+            "spriteCode" : "cooking-equipment-image"
+        },
+        {
+            "id" :"ITEMID_SAV0_item:13",
+            "name" : "Fishing gear",
+            "description" : "Gear used for fishing, let\'s hope you catch the jackpot.",
+            "effectCode" : "",
+            "spriteCode" : "fishing-gear-image"
         }
     ];
 }
--- server/data_init/locations.js
+++ server/data_init/locations.js
@@ -1,5 +1,52 @@
 export function locationsInit() {
     return [
-        
+        {
+            "id" : "LOCATIONID_SAV0_item:0",
+            "name": "Toronto",
+            "description": "As you embark on your journey in 1793, Toronto is a small colonial settlement on the northern shore of Lake Ontario. It's a place where pioneering settlers are building a community, engaging in fur trading, constructing homes, and cultivating the land. It represents the early stages of urban development.",
+            "spriteCode": "toronto-image"
+        },
+        {
+            "id" : "LOCATIONID_SAV0_item:1",
+            "name": "Small Aboriginal Settlement",
+            "description": "Nestled deep in the wilderness, this indigenous settlement is a testament to the rich heritage of the First Nations people. They live off the land, relying on hunting, fishing, and gathering to sustain their way of life. Beyond that, they share their knowledge of the land, its resources, and their spiritual connections with the natural world.",
+            "spriteCode": "small-aboriginal-settlement-image"
+        },
+        {
+            "id" : "LOCATIONID_SAV0_item:2",
+            "name": "Kingston",
+            "description": "Kingston is a vital British military fort, crucial for maintaining security in the region.  The Great Lakes region plays a significant role in the defense of British North America. Beyond its military importance, it's a place where early settlers are working together to establish law and order, education, and infrastructure to support the burgeoning community.",
+            "spriteCode": "kingston-image"
+        },
+        {
+            "id" : "LOCATIONID_SAV0_item:3",
+            "name": "Montreal",
+            "description": "In 1793, Montreal is a bustling hub of commerce that serves for fur trade, attracting trappers, traders who exchange pelts for a variety of goods.. It's also a place where cultural diversity thrives. French and British influences have blended and created a unique cultural tapestry. It's a center for art, literature, and lively debates among the residents.",
+            "spriteCode": "montreal-image"
+        },
+        {
+            "id" : "LOCATIONID_SAV0_item:4",
+            "name": "Three Rivers",
+            "description": "The name \"Three Rivers\" refers to the confluence of the Saint-Maurice and Saint Lawrence Rivers. This junction is essential for the transportation of furs and goods, supporting the economic growth of the region.",
+            "spriteCode": "three-rivers-image"
+        },
+        {
+            "id" : "LOCATIONID_SAV0_item:5",
+            "name": "Quebec",
+            "description": " Quebec City is a fortified city and a hub of French culture, featuring cobblestone streets, fortifications, and a bustling port. It was the heart of New France and plays a vital role in North American history.",
+            "spriteCode": "quebec-image"
+        },
+        {
+            "id" : "LOCATIONID_SAV0_item:6",
+            "name": "Tadoussac",
+            "description": "Tadoussac, at the confluence of two major rivers, is known not only for trade but also for the breathtaking natural beauty. The majestic Saguenay Fjord and the pristine St. Lawrence River offers an opportunity for travelers to marvel at the wonders of the natural world.",            
+            "spriteCode": "tadoussac-image"
+        },
+        {
+            "id" : "LOCATIONID_SAV0_item:7",
+            "name": "Chicoutimi",
+            "description": " As you reach the final destination of Chicoutimi, you'll discover that beyond its role as a fur trading post, this remote settlement is a gateway to the untamed wilderness. It serves as a starting point for intrepid explorers and fur trappers who venture into the vast and untamed landscapes of the Saguenay region.",
+            "spriteCode": "chicoutimi-image"
+        },
     ]
 }

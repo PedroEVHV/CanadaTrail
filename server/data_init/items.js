ce845733e9880fa912e69a3a65d3f0fa20dedc51
Pedro Espinha van Hylckama Vlieg <pevhv.2001@gmail.com>
2023-10-10 21:34:56 -0400

    server data init

--- server/data_init/items.js
+++ server/data_init/items.js
@@ -1,5 +1,10 @@
 export function itemsInit() {
     return [
-
+        {
+            "id" :"ITEMID_SAV0_item:0",
+            "name" : "Water flask",
+            "description" : "",
+            "effectCode" : "i-w-20-1",
+        }
     ];
 }
